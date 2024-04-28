9b0fa745d4f8ce602e2e1d49cd42d70ca8df4ea8
Bowen Jiang <bwjiang@seas.upenn.edu>
2024-04-28 10:51:03 -0400

    data claude

--- run.sh
+++ run.sh
@@ -25,24 +25,6 @@ gpus=(0 1 2 3 4 5 6 7)
 # Loop through the array and run each configuration on a different GPU in the background
 for i in "${!prompting_methods[@]}"; do
     gpu_id=${gpus[$((i % 8))]}
-    CUDA_VISIBLE_DEVICES="$gpu_id" python main.py --model claude --task inference --eval_mode "${prompting_methods[$i]}" --data_file synthetic_dataset_linda_variant_two_because_gold.json &
-done
-
-# Wait for all background jobs to finish
-wait
-
-# Loop through the array and run each configuration on a different GPU in the background
-for i in "${!prompting_methods[@]}"; do
-    gpu_id=${gpus[$((i % 8))]}
-    CUDA_VISIBLE_DEVICES="$gpu_id" python main.py --model claude --task inference --eval_mode "${prompting_methods[$i]}" --data_file synthetic_dataset_linda_variant_two_sothat_gold.json &
-done
-
-# Wait for all background jobs to finish
-wait
-
-# Loop through the array and run each configuration on a different GPU in the background
-for i in "${!prompting_methods[@]}"; do
-    gpu_id=${gpus[$((i % 8))]}
     CUDA_VISIBLE_DEVICES="$gpu_id" python main.py --model claude --task inference --eval_mode "${prompting_methods[$i]}" --data_file synthetic_dataset_linda_variant_two_to_gold.json &
 done
 
