4a23f3ebca76c8c4ea6da2644d25ea469cc15d04
Bowen Jiang <bwjiang@seas.upenn.edu>
2024-06-08 19:28:17 -0400

    upload data

--- run.sh
+++ run.sh
@@ -2,32 +2,31 @@
 
 # Array of prompting methods
 variants=(
-    'linda_original'
-    'linda_variant_one_because'
-    'linda_variant_one_sothat'
-    'linda_variant_one_to'
-    'linda_variant_two_because'
-    'linda_variant_two_sothat'
-    'linda_variant_two_to'
+#    'linda_original'
+#    'linda_variant_one_because'
+#    'linda_variant_one_sothat'
+#    'linda_variant_one_to'
+#    'linda_variant_two_because'
+#    'linda_variant_two_sothat'
+#    'linda_variant_two_to'
     'linda_variant_three'
     'linda_variant_four'
-#    'sets_original'
-#    'sets_original_framing'
+    'sets_original'
+    'sets_original_framing'
 )
 
 llms=(
-  'gpt-3.5-turbo'
-  'gpt-4-turbo'
-  'gpt-4o'
-  'gemini-1.0-pro-002'
-  'gemini-1.5-pro-preview-0409'
-  'llama3-70b'
-  'llama3-8b'
-  'llama-2-70b-chat'
-  'claude-3-opus-20240229'
-  'claude-3-sonnet-20240229'
-  'mistral-large-latest'
-  'open-mixtral-8x22b'
+#    'gpt-3.5-turbo'
+#    'gpt-4-turbo'
+#    'gpt-4o'
+#    'gemini-1.0-pro-002'
+#    'gemini-1.5-pro-preview-0409'
+#    'llama3-70b'
+#    'llama3-8b'
+#    'llama-2-70b-chat'
+    'claude-3-opus-20240229'
+#    'claude-3-sonnet-20240229'
+#    'mistral-large-latest'
 )
 
 prompt=(
@@ -61,23 +60,28 @@ gpus=(4 5 6 7)
 trap 'echo "Terminating all processes..."; kill $(jobs -p); exit' SIGINT SIGTERM
 
 # Loop through the array and run each configuration on a different GPU in the background
-for i in "${!variants[@]}"; do
-    for j in "${!prompt[@]}"; do
-        for k in "${!group[@]}"; do
-            gpu_id=${gpus[$((j % 4))]}
+for l in "${!llms[@]}"; do
+    for i in "${!variants[@]}"; do
+        for j in "${!prompt[@]}"; do
+            # Check if it's one of the last two variants and if the prompt is one of the four to skip
+            if [[ $i -ge $((${#variants[@]} - 2)) ]] && [[ "${prompt[$j]}" == "os_bob" || "${prompt[$j]}" == "os_bob_cot" || "${prompt[$j]}" == "fs_no_linda" || "${prompt[$j]}" == "fs_no_linda_cot" ]]; then
+                continue  # Skip the unwanted prompts for the last two variants
+            fi
 
-            fallacy="linda"  # Default fallacy setting
-#            # Check if it's one of the last two variants
-#            if [[ $i -ge $((${#variants[@]} - 2)) ]]; then
-#                fallacy="sets"
-#            fi
+            for k in "${!group[@]}"; do
+                gpu_id=${gpus[$((j % 4))]}  # Calculate GPU ID based on prompt index
 
-            CUDA_VISIBLE_DEVICES="$gpu_id" python3 main.py --model gpt-4-turbo --fallacy "$fallacy" --task inference --eval_mode "${prompt[$j]}" --data_file synthetic_dataset_"${variants[$i]}"_"${group[$k]}".json &
+                fallacy="linda"  # Default fallacy setting
+                if [[ $i -ge $((${#variants[@]} - 2)) ]]; then
+                    fallacy="sets"
+                fi
+
+                # Execute the task on the chosen GPU
+                CUDA_VISIBLE_DEVICES="$gpu_id" python3 main.py --model "${llms[$l]}" --fallacy "$fallacy" --task inference --eval_mode "${prompt[$j]}" --data_file synthetic_dataset_"${variants[$i]}"_"${group[$k]}".json &
+            done
         done
+        wait  # Wait for all background jobs to finish before moving on to the next variant
     done
-    # Wait for all background jobs to finish before moving on to the next variant
-    wait
+    wait  # Wait for all llm jobs to finish
 done
-
-wait
-
+wait  # Ensure all background jobs have finished before the script exits
