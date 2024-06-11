f3a5b0bef062e5b7adc021bc8aed1f549060142a
Bowen Jiang <bwjiang@seas.upenn.edu>
2024-06-10 23:45:50 -0400

    add more data

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Starting script"
 # Array of prompting methods
 variants=(
 #    'linda_original'
@@ -11,8 +12,8 @@ variants=(
 #    'linda_variant_two_to'
 #    'linda_variant_three'
 #    'linda_variant_four'
-#    'sets_original'
-    'sets_original_framing'
+    'sets_original'
+#    'sets_original_framing'
 )
 
 llms=(
@@ -34,18 +35,18 @@ prompt=(
     'zs_cot'
     'os'
     'os_cot'
-    'os_bob'
-    'os_bob_cot'
-    'os_incorrect'
-    'os_incorrect_cot'
+#    'os_bob'
+#    'os_bob_cot'
+#    'os_incorrect'
+#    'os_incorrect_cot'
     'fs'
     'fs_cot'
-    'fs_no_linda'
-    'fs_no_linda_cot'
-    'weak_control_zs_cot'
-    'weak_control_os_cot'
-    'control_zs_cot'
-    'control_os_cot'
+#    'fs_no_linda'
+#    'fs_no_linda_cot'
+#    'weak_control_zs_cot'
+#    'weak_control_os_cot'
+#    'control_zs_cot'
+#    'control_os_cot'
 )
 
 
@@ -56,6 +57,7 @@ group=(
 
 gpus=(4 5 6 7)
 
+
 # Setup a trap to handle SIGINT and SIGTERM
 trap 'echo "Terminating all processes..."; kill $(jobs -p); exit' SIGINT SIGTERM
 
@@ -64,17 +66,17 @@ for l in "${!llms[@]}"; do
     for i in "${!variants[@]}"; do
         for j in "${!prompt[@]}"; do
             # Check if it's one of the last two variants and if the prompt is one of the four to skip
-            if [[ $i -ge $((${#variants[@]} - 2)) ]] && [[ "${prompt[$j]}" == "os_bob" || "${prompt[$j]}" == "os_bob_cot" || "${prompt[$j]}" == "fs_no_linda" || "${prompt[$j]}" == "fs_no_linda_cot" ]]; then
-                continue  # Skip the unwanted prompts for the last two variants
-            fi
+#            if [[ $i -ge $((${#variants[@]} - 2)) ]] && [[ "${prompt[$j]}" == "os_bob" || "${prompt[$j]}" == "os_bob_cot" || "${prompt[$j]}" == "fs_no_linda" || "${prompt[$j]}" == "fs_no_linda_cot" ]]; then
+#                continue  # Skip the unwanted prompts for the last two variants
+#            fi
 
             for k in "${!group[@]}"; do
                 gpu_id=${gpus[$((j % 4))]}  # Calculate GPU ID based on prompt index
 
-                fallacy="linda"  # Default fallacy setting
-                if [[ $i -ge $((${#variants[@]} - 2)) ]]; then
-                    fallacy="sets"
-                fi
+                fallacy="sets"  # Default fallacy setting
+#                if [[ $i -ge $((${#variants[@]} - 2)) ]]; then
+#                    fallacy="sets"
+#                fi
 
                 # Execute the task on the chosen GPU
                 CUDA_VISIBLE_DEVICES="$gpu_id" python3 main.py --model "${llms[$l]}" --fallacy "$fallacy" --task inference --eval_mode "${prompt[$j]}" --data_file synthetic_dataset_"${variants[$i]}"_"${group[$k]}".json &
@@ -85,3 +87,5 @@ for l in "${!llms[@]}"; do
     wait  # Wait for all llm jobs to finish
 done
 wait  # Ensure all background jobs have finished before the script exits
+
+echo "Finishing script"
