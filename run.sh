33b65abac25be683dd676b745c7f6ed56a75ef10
Bowen Jiang <bwjiang@seas.upenn.edu>
2024-06-12 12:08:43 -0400

    add data

--- run.sh
+++ run.sh
@@ -11,23 +11,23 @@ variants=(
 #    'linda_variant_two_sothat'
 #    'linda_variant_two_to'
 #    'linda_variant_three'
-#    'linda_variant_four'
-    'sets_original'
+    'linda_variant_four'
+#    'sets_original'
 #    'sets_original_framing'
 )
 
 llms=(
-#    'gpt-3.5-turbo'
-#    'gpt-4-turbo'
-#    'gpt-4o'
+    'gpt-3.5-turbo'
+    'gpt-4-turbo'
+    'gpt-4o'
 #    'gemini-1.0-pro-002'
 #    'gemini-1.5-pro-preview-0409'
     'llama3-70b'
-#    'llama3-8b'
-#    'llama-2-70b-chat'
-#    'claude-3-opus-20240229'
-#    'claude-3-sonnet-20240229'
-#    'mistral-large-latest'
+    'llama3-8b'
+    'llama-2-70b-chat'
+    'claude-3-opus-20240229'
+    'claude-3-sonnet-20240229'
+    'mistral-large-latest'
 )
 
 prompt=(
@@ -37,10 +37,10 @@ prompt=(
 #    'os_cot'
 #    'os_bob'
 #    'os_bob_cot'
-#    'os_incorrect'
-#    'os_incorrect_cot'
+    'os_incorrect'
+    'os_incorrect_cot'
 #    'fs'
-    'fs_cot'
+#    'fs_cot'
 #    'fs_no_linda'
 #    'fs_no_linda_cot'
 #    'weak_control_zs_cot'
@@ -51,8 +51,8 @@ prompt=(
 
 
 group=(
-#    'gold'
-    'random'
+    'gold'
+#    'random'
 )
 
 gpus=(4 5 6 7)
